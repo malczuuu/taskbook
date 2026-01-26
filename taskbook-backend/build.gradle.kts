92108f07aa6a7e726d5f502304ccc2258f8d73e1
Damian Malczewski <damian.m.malczewski@gmail.com>
2026-01-26 20:27:42 +0100

    Use webmvc instead of web

--- taskbook-backend/build.gradle.kts
+++ taskbook-backend/build.gradle.kts
@@ -27,7 +27,7 @@ dependencies {
     implementation(libs.spring.boot.starter.data.jpa)
     implementation(libs.spring.boot.starter.security)
     implementation(libs.spring.boot.starter.validation)
-    implementation(libs.spring.boot.starter.web)
+    implementation(libs.spring.boot.starter.webmvc)
 
     implementation(libs.micrometer.registry.prometheus)
 
